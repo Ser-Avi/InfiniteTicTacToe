b4cd1386fe6f146f72964f9aa00f92b34c2e448b
AVI_SEREBRENIK <avi_serebrenik@yahoo.com>
2023-03-25 23:17:09 -0400

    Finished Step 2

--- InfTicTac.py
+++ InfTicTac.py
@@ -5,9 +5,51 @@ The base tic-tac-toe game was found in the following guide: https://realpython.c
 """
 
 import tkinter as tk
+from itertools import cycle
 from tkinter import font
+from typing import NamedTuple
 
-gameSize = 3
+class Player(NamedTuple):   #player class
+    label: str  #store x or o
+    color:str   #color for player indentification
+
+class Move(NamedTuple):     #class for each move
+    row:int     #coordinates of move
+    col:int
+    label:str = ""  #whether move is legal
+
+BOARD_SIZE = 3
+DEFAULT_PLAYERS = (Player(label="x", color="blue"),
+                   Player(label="O", color="green"))
+
+class TicTacToeGame:
+    def __init__(self, players:DEFAULT_PLAYERS, board_size = BOARD_SIZE):
+        self._players = cycle(players)  #cycles over player tuple
+        self.board_size = board_size    
+        self.current_player = next(self.__players)
+        self.winner_combo=[]        #combo that defines winner
+        self._current_moves = []    #list of player moves
+        self._has_winner = False    #if win
+        self._winning.combos = []   #list with combos that make a win
+        self._setup_board()
+    
+    def _setup_board(self):
+        self._current_moves = [[Move(row, col) for col in range(self.board_size)]
+                               for row in range(self.board_size)]   #initial list of player moves
+        self._winning_combos = self._get_winning_combos()
+    
+    def _get_winning_combos(self):
+        rows = [
+            [(move.row, move.col) for move in row]
+            for row in self._current_moves
+        ]
+        columns = [list(col) for col in zip(*rows)]
+        first_diagonal = [row[i] for i, row in enumerate(rows)]
+        second_diagonal = [col[j] for j, col in enumerate(reversed(columns))]
+        return rows + columns + [first_diagonal, second_diagonal]
+
+
+gameSize = 4
 
 class TicTacToeBoard(tk.Tk):                #class inherits from Tk
     def __init__(self):
